35a7ff7f111108101d9ed518d94d96dec366b596
github-action-benchmark <github@users.noreply.github.com>
2026-05-01 21:43:04 +0000

    add Benchmark (googlecpp) benchmark result for 4136801d3ef5e881f0012fbf7153816f01825927

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1777671784763,
+  "repoUrl": "https://github.com/Marbjo07/NForge",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "64412113+Marbjo07@users.noreply.github.com",
+            "name": "Marbjo07",
+            "username": "Marbjo07"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4136801d3ef5e881f0012fbf7153816f01825927",
+          "message": "Merge pull request #40 from Marbjo07/feat/Benchmarking\n\nfeat: benchmark physics tests",
+          "timestamp": "2026-05-01T23:40:43+02:00",
+          "tree_id": "881bff3b2174fae8339be3ebfa9c8cdda4c3ca58",
+          "url": "https://github.com/Marbjo07/NForge/commit/4136801d3ef5e881f0012fbf7153816f01825927"
+        },
+        "date": 1777671783586,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_ProjectileMotion",
+            "value": 509892.3310000032,
+            "unit": "ns/iter",
+            "extra": "iterations: 1000\ncpu: 509880.4540000001 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_SphereSlide",
+            "value": 15841676.636363579,
+            "unit": "ns/iter",
+            "extra": "iterations: 44\ncpu: 15838535.818181818 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
