3ac474fbb28689e5c2f3f1e6c4e482b5370166e2
Marbjo07 <marius.bjorhei@gmail.com>
2026-05-01 23:02:47 +0200

    feat: benchmark physics tests

---
+++ benchmarks/bench_physics.cpp
@@ -0,0 +1,22 @@
+#include <benchmark/benchmark.h>
+#include "physics/projectile_motion.h"
+#include "physics/sphere_slide.h"
+
+static void BM_ProjectileMotion(benchmark::State& state) {
+    ProjectileMotionParams params;
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(simulateProjectileMotion(params));
+    }
+}
+
+static void BM_SphereSlide(benchmark::State& state) {
+    SphereSlideParams params;
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(simulateSphereSlide(params));
+    }
+}
+
+
+BENCHMARK(BM_ProjectileMotion);
+BENCHMARK(BM_SphereSlide);
+
