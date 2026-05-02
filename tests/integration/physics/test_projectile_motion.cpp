300e5857c8a13748b805418a8f49278c36d85556
Marbjo07 <marius.bjorhei@gmail.com>
2026-05-02 18:08:26 +0200

    feat: check approx equal physics tests

--- tests/integration/physics/test_projectile_motion.cpp
+++ tests/integration/physics/test_projectile_motion.cpp
@@ -7,7 +7,6 @@
 #include "utils.h"
 
 
-
 TEST_CASE("Diagonal throw", "[Physics]") {
     ProjectileMotionParams params;
     params.angle = 45; // degrees
@@ -16,8 +15,12 @@ TEST_CASE("Diagonal throw", "[Physics]") {
 
     ProjectileMotionResults res = simulateProjectileMotion(params);
 
-    REQUIRE(res.position == create2dVector(10.189493179321289, -0.002771137747914));
-    REQUIRE(res.speed == create2dVector(7.071067810058594, -7.065103530883789));
+    INFO(res.position.getDataString());
+    INFO(res.speed.getDataString());
+    INFO(res.t);
+
+    REQUIRE(isSimilar(res.position, create2dVector(10.189493179321289, -0.002771137747914)));
+    REQUIRE(isSimilar(res.speed, create2dVector(7.071067810058594, -7.065103530883789)));
     REQUIRE(res.t == 1.44101131f);
 }
 
@@ -28,7 +31,11 @@ TEST_CASE("Vertical throw", "[Physics]") {
 
     ProjectileMotionResults res = simulateProjectileMotion(params);
 
-    REQUIRE(res.position == create2dVector(-0.000000890855176, -0.002778571099043));
-    REQUIRE(res.speed == create2dVector(-0.000000437113897, -9.992918014526367));
+    INFO("position: " << res.position.getDataString());
+    INFO("speed: " << res.speed.getDataString());
+    INFO("t: " << res.t);
+
+    REQUIRE(isSimilar(res.position, create2dVector(-0.000000890855176, -0.002778571099043)));
+    REQUIRE(isSimilar(res.speed, create2dVector(-0.000000437113897, -9.992918014526367)));
     REQUIRE(res.t == 2.038034677505493);
 }
