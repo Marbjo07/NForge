0e92cefe81a48a1853216c8a0505a0f848ffdb2f
Marbjo07 <marius.bjorhei@gmail.com>
2026-05-15 23:18:33 +0200

    fix: formating physics bench

--- benchmarks/bench_physics.cpp
+++ benchmarks/bench_physics.cpp
@@ -1,19 +1,20 @@
 #include <benchmark/benchmark.h>
+
 #include "physics/projectile_motion.h"
 #include "physics/sphere_slide.h"
 
 static void BM_ProjectileMotion(benchmark::State& state) {
-    ProjectileMotionParams params;
-    for (auto _ : state) {
-        benchmark::DoNotOptimize(simulateProjectileMotion(params));
-    }
+	ProjectileMotionParams params;
+	for (auto _ : state) {
+		benchmark::DoNotOptimize(simulateProjectileMotion(params));
+	}
 }
 
 static void BM_SphereSlide(benchmark::State& state) {
-    SphereSlideParams params;
-    for (auto _ : state) {
-        benchmark::DoNotOptimize(simulateSphereSlide(params));
-    }
+	SphereSlideParams params;
+	for (auto _ : state) {
+		benchmark::DoNotOptimize(simulateSphereSlide(params));
+	}
 }
 
 
