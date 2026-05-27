501405cc6b55ebee2c3454aa023767bef4bea534
Marbjo07 <marius.bjorhei@gmail.com>
2026-05-27 17:03:57 +0200

    remove performace regression trigger

--- src/core/tensor.cpp
+++ src/core/tensor.cpp
@@ -115,15 +115,10 @@ template <typename BinaryOp>
 Tensor Tensor::applyBinaryOp(const Tensor::View& rhs, BinaryOp op) const {
 	auto ctx = semantic::BinaryOpContext::build(*this, rhs);
 
-	// trigger performance regression
-	Tensor ret = Tensor(ctx.out);
-	for (size_t i = 0; i < 10; i++) {
-		Tensor::Impl* rhsImpl = rhs.getParent().m_impl.get();
-		auto result = (m_impl.get()->*op)(ctx.lhs, rhsImpl, ctx.rhs, ctx.out);
-		ret = Tensor(std::move(result), m_backend);
-	}
+	Tensor::Impl* rhsImpl = rhs.getParent().m_impl.get();
+	auto result = (m_impl.get()->*op)(ctx.lhs, rhsImpl, ctx.rhs, ctx.out);
 
-	return ret;
+	return Tensor(std::move(result), m_backend);
 }
 
 Tensor Tensor::operator+(const Tensor::View& rhs) const {
